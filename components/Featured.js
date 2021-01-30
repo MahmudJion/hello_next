1aea05404e3cfc4a4b9290f6dbfb28e0f5f9f595
MahmudJion <mahmudh.jion02@gmail.com>
2021-01-30 21:55:40 +0600

    package update and server port changed for build version

--- components/Featured.js
+++ components/Featured.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 class Featured extends React.Component {
 
   render() {
--- components/Trending.js
+++ components/Trending.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 class Trending extends React.Component {
 	state = {
 		trending: []
