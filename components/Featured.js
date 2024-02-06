d136afcaa830a7aaa8545d9595cb15e99e804dff
Mahmud Hasan Jion <mahmudh.jion02@gmail.com>
2024-02-06 00:22:44 +0000

    Refactor Featured component to use functional component and improve destructuring
    - Converted the Featured component from a class component to a functional component for simplicity.
    - Utilized array destructuring in the function parameter list for cleaner code.
    - Removed unnecessary class-related syntax since it's not needed for this component.

--- components/Featured.js
+++ components/Featured.js
@@ -1,28 +1,20 @@
 import React from 'react';
 
-class Featured extends React.Component {
-
-  render() {
-
-    return (
-      <div>
-		<table className="table table-hover">
-			{this.props.featured.map((post, index) => {
-				return (
-				<tbody key={index}>
-					<tr>
-					<td>
-						<h4>{post.title}</h4>
-						<p>{post.body}</p>
-					</td>
-					</tr>
-				</tbody>
-				)
-			})}
-		</table>
-      </div>
-    );
-  }
-}
+const Featured = ({ featured }) => (
+  <div>
+    <table className="table table-hover">
+      {featured.map(({ title, body }, index) => (
+        <tbody key={index}>
+          <tr>
+            <td>
+              <h4>{title}</h4>
+              <p>{body}</p>
+            </td>
+          </tr>
+        </tbody>
+      ))}
+    </table>
+  </div>
+);
 
 export default Featured;
