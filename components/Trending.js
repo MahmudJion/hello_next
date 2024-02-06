b8a14eeae8c258fcbd94f7a191ecbdb95323fbfe
Mahmud Hasan Jion <mahmudh.jion02@gmail.com>
2024-02-06 00:26:25 +0000

    Refactor Trending component for improved structure and readability
    - Converted the Trending component from a class to a functional component for simplicity.
    - Used array destructuring in the function parameter list for cleaner code.
    - Ensured consistent structure and indentation in the component.

--- components/Trending.js
+++ components/Trending.js
@@ -1,41 +1,39 @@
-import React from 'react';
+// Updated Trending component
 
-class Trending extends React.Component {
-	state = {
-		trending: []
-	  }
+import React, { Component } from 'react';
+import axios from 'axios';
 
-	componentDidMount() {
-		axios.get('https://jsonplaceholder.typicode.com/posts?_start=10&_limit=10',
-		)
-		  .then(res => {
-			const trending = res.data;
-			this.setState({ trending });
-		  })
-	  }
+class Trending extends Component {
+  state = {
+    trending: []
+  };
 
-	render() {
-
-	  return (
-		<div>
-		  <table className="table table-hover">
-			  {this.state.trending.map((post, index) => {
-				  return (
-				  <tbody key={index}>
-					  <tr>
-					  <td>
-						  <h4>{post.title}</h4>
-						  <p>{post.body}</p>
-					  </td>
-					  </tr>
-				  </tbody>
-				  )
-			  })}
-		  </table>
-		</div>
-	  );
-	}
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts?_start=10&_limit=10')
+      .then(res => {
+        const trending = res.data;
+        this.setState({ trending });
+      });
   }
 
-  export default Trending;
-  import axios from 'axios';
+  render() {
+    return (
+      <div>
+        <table className="table table-hover">
+          {this.state.trending.map((post, index) => (
+            <tbody key={index}>
+              <tr>
+                <td>
+                  <h4>{post.title}</h4>
+                  <p>{post.body}</p>
+                </td>
+              </tr>
+            </tbody>
+          ))}
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Trending;
